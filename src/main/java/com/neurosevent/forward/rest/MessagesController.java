6396023d8c60331047a0441b2b08ce8fb3afc3d0
Matteo Balzerani <matteo.balzerani@gmail.com>
2020-04-25 19:42:48 +0200

    fix configuration and create consumer on rest call

--- src/main/java/com/neurosevent/forward/rest/MessagesController.java
+++ src/main/java/com/neurosevent/forward/rest/MessagesController.java
@@ -1,19 +1,24 @@
 package com.neurosevent.forward.rest;
 
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.PartitionInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.neurosevent.forward.config.KafkaProperties;
 
 @RestController
 @RequestMapping("/api/messages")
@@ -21,18 +26,62 @@ public class MessagesController {
 
 	private final Logger log = LoggerFactory.getLogger(MessagesController.class);
 
-//	@GetMapping("/consume")
-//	public SseEmitter consume(@RequestBody List<String> topics, @RequestParam Map<String, String> consumerParams) {
-//		log.debug("REST request to consume records from Kafka topics {}", topics);
-//		Map<String, Object> consumerProps = kafkaProperties.getConsumerProps();
-//		consumerProps.putAll(consumerParams);
-//		consumerProps.remove("topic");
-//	}
+	private final KafkaProperties kafkaProperties;
+	private ExecutorService sseExecutorService = Executors.newCachedThreadPool();
 
-	@KafkaListener(topics = "test1", clientIdPrefix = "string", containerFactory = "kafkaListenerStringContainerFactory")
-	public void listenasString(ConsumerRecord<String, String> cr, @Payload String payload) {
-		log.info("READ [String] received key {}: Type [{}] | Payload: {} | Record: {}", cr.key(), payload,
-				cr.toString());
+	public MessagesController(KafkaProperties kafkaProperties) {
+		this.kafkaProperties = kafkaProperties;
+	}
+
+	@GetMapping("/consume")
+	public SseEmitter consume() {
+		log.debug("REST request to consume records from Kafka all topics");
+		Map<String, Object> consumerProps = kafkaProperties.getConsumerProps();
+
+		SseEmitter emitter = new SseEmitter(0L);
+		sseExecutorService.execute(() -> {
+			KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProps);
+			List<String> listTopics;
+			try {
+				listTopics = retrieveAllTopics(consumer);
+
+				emitter.onCompletion(consumer::close);
+				consumer.subscribe(listTopics);
+				boolean exitLoop = false;
+				while (!exitLoop) {
+					try {
+						List<String> listTopicsNew = retrieveAllTopics(consumer);
+						if (!listTopicsNew.equals(listTopics)) {
+							consumer.subscribe(listTopicsNew);
+						}
+						ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(5));
+						for (ConsumerRecord<String, String> record : records) {
+							emitter.send(record.value());
+							log.info(record.value());
+						}
+						emitter.send(SseEmitter.event().comment(""));
+					} catch (Exception ex) {
+						log.trace("Complete with error {}", ex.getMessage(), ex);
+						emitter.completeWithError(ex);
+						exitLoop = true;
+					}
+				}
+				consumer.close();
+				emitter.complete();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		});
+		return emitter;
+
+	}
+
+	private List<String> retrieveAllTopics(KafkaConsumer<String, String> consumer) throws Exception {
+		Map<String, List<PartitionInfo>> topics = consumer.listTopics();
+		List<String> listTopics = new ArrayList<String>(topics.keySet());
+		listTopics.removeIf(x -> x.startsWith("__"));
+		return listTopics;
 	}
 
 }
--- src/main/java/com/neurosevent/forward/service/MessageConsumer.java
+++ src/main/java/com/neurosevent/forward/service/MessageConsumer.java
@@ -1,38 +1,13 @@
 package com.neurosevent.forward.service;
 
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.stereotype.Service;
-
-import com.neurosevent.forward.config.KafkaProperties;
 
 @Service
 public class MessageConsumer {
 
 	private final Logger log = LoggerFactory.getLogger(MessageConsumer.class);
 
-	private final KafkaProperties kafkaProperties;
-
-	public MessageConsumer(KafkaProperties kafkaProperties) {
-		this.kafkaProperties = kafkaProperties;
-	}
-
-	@Bean
-	public ConsumerFactory<String, String> stringConsumerFactory() {
-		return new DefaultKafkaConsumerFactory<>(kafkaProperties.getConsumerProps(), new StringDeserializer(),
-				new StringDeserializer());
-	}
-
-	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerStringContainerFactory() {
-		ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-		factory.setConsumerFactory(stringConsumerFactory());
-
-		return factory;
-	}
+//	PUT HERE LOGIC
 }
